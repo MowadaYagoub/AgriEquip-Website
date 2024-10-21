bb3345888fa6e87b39f92f52f471ead2427495c0
MowadaYagoub <mowadayagoub@gmail.com>
2024-10-21 16:56:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,11 +8,7 @@ let nCount = selector => {
         duration: 4000,
         // A string indicating which easing function to use for the transition.
         easing: "swing",
-        /**
-         * A function to be called for each animated property of each animated element. 
-         * This function provides an opportunity to
-         *  modify the Tween object to change the value of the property before it is set.
-         */
+        
         step: function (value) {
           $(this).text(Math.ceil(value));
         }
@@ -32,11 +28,6 @@ $(window).scroll(function () {
 
 
 
-/**
- *
- *  sticky navigation
- *
- */
 
 let navbar = $(".navbar");
 
